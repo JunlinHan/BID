c509fa64b3dec082737567936d34a856015386c8
JunlinHan <48272064+JunlinHan@users.noreply.github.com>
2021-10-04 17:28:50 +1100

    Update train_fid.py

--- train_fid.py
+++ train_fid.py
@@ -124,7 +124,6 @@ if __name__ == '__main__':
                 batch_size=50, cuda='0', dims=2048)
             visualizer.print_current_fid(epoch, fid_value)
             visualizer.plot_current_fid(epoch, fid_value)
-            model.train()
 
         print('End of epoch %d / %d \t Time Taken: %d sec' % (
             epoch, opt.n_epochs + opt.n_epochs_decay, time.time() - epoch_start_time))
